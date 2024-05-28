af440c7bc93631a11c747057af65ab7fc625713b
Milana Wood <mrsmilanawood@gmail.com>
2024-05-28 13:23:29 +0100

    moved files into the correct directory

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      </header>
+    <div className="App" >
+      {/* Add search bar component*/}
+
+      <div className="App-playlist">
+        {/* Add search results component*/}
+        {/* Add playlist component*/}
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
