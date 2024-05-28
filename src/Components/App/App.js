edfdcfb572ef874a986c59494a5661082257d75d
Milana Wood <mrsmilanawood@gmail.com>
2024-05-28 14:05:32 +0100

    style
    clear

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,21 @@
 import React from "react";
-import './App.css';
+import styles from "./App.css";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
+import Track from "../Track/Track";
+import Tracklist from "../Tracklist/Tracklist";
 
 function App() {
   return (
     <div>
-      <header className="App-header">
+      <header className={styles["App-header"]}>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
       </header>
-    <div className="App" >
-      {/* Add search bar component*/}
+    <div className={styles.App} >
+      <SearchBar />
 
-      <div className="App-playlist">
+      <div className={styles["App-Playlist"]}>
         {/* Add search results component*/}
         {/* Add playlist component*/}
       </div>
